d8575ee09f5543ece41975fd589975e9bbd52227
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-09 11:55:46 -0400

    fixed tilemap offset, springs reset

--- scripts/Spring.gd
+++ scripts/Spring.gd
@@ -1,11 +1,21 @@
 extends Area2D
 
+var reset_duration = 2 # time to wait before reset
+var timer_elapsed = 0.0
 
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
+	var playerCollided = false
+
 	for body in bodies:
 		if body.name == "Player":
-			$AnimationPlayer.play("active")
-			yield($AnimationPlayer, "animation_finished")
-		else:
+			playerCollided = true
+			break
+
+	if playerCollided:
+		$AnimationPlayer.play("active")
+		timer_elapsed = 0.0 
+	else:
+		timer_elapsed += delta  # Increment timer when the player is not overlapping
+		if timer_elapsed >= reset_duration:
 			$AnimationPlayer.play("idle")
