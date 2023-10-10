6ef2bad2bce27507e585b225c7eead4ff1971561
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-09 22:48:17 -0400

    diff size asteroids; no meteroids

--- scripts/World.gd
+++ scripts/World.gd
@@ -1,38 +1,34 @@
 extends Node2D
 
-export(PackedScene) var meteoroid_scene
+#export(PackedScene) var meteoroid_scene
 
-func game_over():
-	$MeteoroidTimer.stop()
-
-func new_game():
-	$StartTimer.start()
+#func game_over():
+#	$MeteoroidTimer.stop()
+#
+#func new_game():
+#	$StartTimer.start()
 	
-func _ready():
-	randomize()	
-	new_game()
+#func _ready():
+#	randomize()	
+#	new_game()
 
+#
+#func _on_StartTimer_timeout():
+#	$MeteoroidTimer.start()
 
-func _on_StartTimer_timeout():
-	$MeteoroidTimer.start()
-
-func _on_MeteoroidTimer_timeout():
-	var meteoroid = meteoroid_scene.instance()
-
-	var random_x = int(get_viewport_rect().size.x)
-	var random_y = -int(get_viewport_rect().size.y)
-	
-	var meteoroid_spawn_location = Vector2(random_x, random_y)
-	meteoroid.position = meteoroid_spawn_location
-	
-	# Set the meteoroid's direction to move downward.
-	var direction = PI / 4
-	meteoroid.rotation = direction
-
-	var velocity = Vector2(0.0, rand_range(350.0, 650.0))
-	meteoroid.linear_velocity = velocity.rotated(direction)
-  
-	var gravity_acceleration = 9.8
-	meteoroid.linear_velocity.y -= gravity_acceleration
-
-	add_child(meteoroid)
+#func _on_MeteoroidTimer_timeout():
+#	var meteoroid = meteoroid_scene.instance()
+#	var meteoroid_spawn_location = get_node("MeteoroidPath/MeteoroidSpawnLocation")
+#	meteoroid_spawn_location.offset = randi()
+#
+#	var direction = meteoroid_spawn_location.rotation + PI / 2
+#	meteoroid.position = meteoroid_spawn_location.position
+#	meteoroid.rotation = direction
+#
+#	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+#	meteoroid.linear_velocity = velocity.rotated(direction)
+##
+#	var gravity_acceleration = 9.8
+#	meteoroid.linear_velocity.y -= gravity_acceleration
+#
+#	add_child(meteoroid)
