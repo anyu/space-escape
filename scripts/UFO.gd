3080414212d3119712a5596d0c109ab3509a44e8
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-12 12:07:31 -0400

    blerrgh

--- scripts/UFO.gd
+++ scripts/UFO.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+var speed = 150
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
--- scripts/World.gd
+++ scripts/World.gd
@@ -1,34 +1,36 @@
 extends Node2D
 
-#export(PackedScene) var meteoroid_scene
+export(PackedScene) var ufo_scene
 
-#func game_over():
-#	$MeteoroidTimer.stop()
-#
-#func new_game():
-#	$StartTimer.start()
+func game_over():
+	$UFOTimer.stop()
+
+func new_game():
+	$StartTimer.start()
+
+func _ready():
+	randomize()	
+	new_game()
+
+func _on_StartTimer_timeout():
+	$UFOTimer.start()
+
+func _on_UFOTimer_timeout():
+	var ufo = ufo_scene.instance()
+	ufo.position = Vector2(320, 320)
+	var ufo_spawn_location = get_node("UFOPath/UFOSpawnLocation")
+	ufo_spawn_location.offset = randi()
+	var direction = ufo_spawn_location.rotation + PI / 2
+	ufo.position = ufo_spawn_location.position
+	direction += rand_range(-PI / 4, PI / 4)
+
+	ufo.rotation = direction
+	print("position", ufo.position)
+	print("rotation", ufo.rotation)
 	
-#func _ready():
-#	randomize()	
-#	new_game()
+	var velocity = Vector2(rand_range(200,400), 0.0)
+	ufo.linear_velocity = velocity.rotated(direction)
+	
+	add_child(ufo)
 
-#
-#func _on_StartTimer_timeout():
-#	$MeteoroidTimer.start()
 
-#func _on_MeteoroidTimer_timeout():
-#	var meteoroid = meteoroid_scene.instance()
-#	var meteoroid_spawn_location = get_node("MeteoroidPath/MeteoroidSpawnLocation")
-#	meteoroid_spawn_location.offset = randi()
-#
-#	var direction = meteoroid_spawn_location.rotation + PI / 2
-#	meteoroid.position = meteoroid_spawn_location.position
-#	meteoroid.rotation = direction
-#
-#	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
-#	meteoroid.linear_velocity = velocity.rotated(direction)
-##
-#	var gravity_acceleration = 9.8
-#	meteoroid.linear_velocity.y -= gravity_acceleration
-#
-#	add_child(meteoroid)
