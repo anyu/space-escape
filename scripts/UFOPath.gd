afd27d8883e0ff484280869af17296bbfb99fe5d
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-12 23:10:03 -0400

    WIP

--- scripts/UFOPath.gd
+++ scripts/UFOPath.gd
@@ -1,9 +1,16 @@
 extends Path2D
 
 onready var follow = get_node("UFOPathFollow")
+var cameraOffset = Vector2(0, 700)
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
+	var world = get_parent()
+	var camera = world.get_node("Camera2D")
+	var cameraGlobalPosition = camera.global_position
+	
+	# Set the Path2D's global position to the camera's position plus the offset
+	follow.global_position = cameraGlobalPosition 
 	follow.set_offset(follow.get_offset() + 350 * delta)
