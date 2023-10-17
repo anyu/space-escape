5fb6b4de047899ff60acce22a0a0d74a1f518c78
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-17 11:03:25 -0400

    randomize lasers

--- scripts/UFO.gd
+++ scripts/UFO.gd
@@ -1,5 +1,8 @@
 extends Area2D
-var MOVE_SPEED = 250
+
+const MOVE_SPEED = 250
+const MIN_LASER_WAIT_SECS = 0.2
+const MAX_LASER_WAIT_SECS = 1.5
 
 var laser_scene = preload("res://scenes/Laser.tscn")
 onready var sprite = $Sprite
@@ -16,6 +19,9 @@ func _on_LaserTimer_timeout():
 	add_child(laser)
 	var ufo_height = sprite.texture.get_height()
 	laser.global_position = global_position + Vector2(0, ufo_height + 10)
+	var random_secs = randf() * (MAX_LASER_WAIT_SECS - MIN_LASER_WAIT_SECS) + MIN_LASER_WAIT_SECS
+	$LaserTimer.wait_time = random_secs
+	$LaserTimer.start()	
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
