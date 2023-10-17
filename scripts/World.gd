42d9a466d0c4a0a99ccfc222b3034ed6d38d9c72
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-17 10:51:35 -0400

    ufos randomized

--- scripts/World.gd
+++ scripts/World.gd
@@ -3,6 +3,8 @@ extends Node2D
 export(PackedScene) var ufo_scene
 export(PackedScene) var laser_scene
 
+const MIN_UFO_WAIT_SECS = 1.0
+const MAX_UFO_WAIT_SECS = 8.0
 
 onready var camera = get_node("Camera2D")
 
@@ -20,8 +22,12 @@ func _on_UFOTimer_timeout():
 	ufo.position.x = 0
 	ufo.position.y = camera.global_position.y
 	add_child(ufo)
+
+	var random_secs = randf() * (MAX_UFO_WAIT_SECS - MIN_UFO_WAIT_SECS) + MIN_UFO_WAIT_SECS
+	$UFOTimer.wait_time = random_secs
+	$UFOTimer.start()
 	
 func end_game():
 	$UFOTimer.stop()
-	yield(get_tree().create_timer(2.5), "timeout")	
+	yield(get_tree().create_timer(2.5), "timeout")
 	get_tree().change_scene("res://scenes/GameOver.tscn")
