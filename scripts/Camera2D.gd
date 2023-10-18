ae1f2dbfc4b0c5f7bddb51720aeeaa0d4276384d
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-18 11:25:04 -0400

    delay camera start

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -2,6 +2,10 @@ extends Camera2D
 
 
 var MOVE_SPEED = 150
+var STARTING_POSITION = 400
+
+func _ready():
+	position.y = STARTING_POSITION
 
 func _process(delta):
 	position.y -= MOVE_SPEED * delta
